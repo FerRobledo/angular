f4916ed9aae19d253c1ff3867d740a8585a9e74a
Fermin Robledo <fermin4robledo@gmail.com>
2024-12-10 18:14:50 -0300

    Implementacion mini-calculadora

--- calculadora/src/app/app.component.ts
+++ calculadora/src/app/app.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'calculadora';
+  title = 'Mini Calculadora';
+  numero1: number=0;
+  numero2: number=0;
+  resultado: number=0;
+  hayResultado: boolean=false;
+
+
+  sumar():void {
+    this.resultado = this.numero1+this.numero2;
+  }
+
+  restar():void{
+    this.resultado = this.numero1-this.numero2;
+  }
+
+  multiplicar():void{
+    this.resultado = this.numero1*this.numero2;
+  }
 }
